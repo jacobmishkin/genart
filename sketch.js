7e362766bfac6b36035ed93a44aa78497971fa75
Jacob Mishkin <user@MacBook-Pro-3.local>
2019-02-14 15:44:43 -0600

    added color palettes

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const canvasSketch = require("canvas-sketch");
 const { lerp } = require("canvas-sketch-util/math");
 const random = require("canvas-sketch-util/random");
-
+const palettes = require("nice-color-palettes");
 const settings = {
   dimensions: [2048, 2048],
   orientation: "landscape"
@@ -9,34 +9,42 @@ const settings = {
 
 const sketch = () => {
   const createGrid = () => {
+    const colorCount = random.rangeFloor(2, 6);
+    const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount);
+    console.log(palette);
     const points = [];
-    const count = 60;
+    const count = 20;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
-        points.push([u, v]);
+        points.push({
+          color: random.pick(palette),
+          radius: Math.abs(0.01 + random.gaussian() * 0.01),
+          position: [u, v]
+        });
       }
     }
     return points;
   };
 
-  random.setSeed(10);
+  // random.setSeed(10);
   const points = createGrid().filter(() => random.value() > 0.5);
-  const margin = 400;
+  const margin = 200;
 
   return ({ context, width, height }) => {
-    context.fillStyle = "#bd6867";
+    context.fillStyle = "salmon";
     context.fillRect(0, 0, width, height);
 
-    points.forEach(([u, v]) => {
+    points.forEach(data => {
+      const { position, radius, color } = data;
+      const [u, v] = position;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 5, 0, Math.PI * 2, false);
-      context.fillStyle = "#a2eae2";
-      context.lineWidth = 20;
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      context.fillStyle = color;
       context.fill();
     });
   };
