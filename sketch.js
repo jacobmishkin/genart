966d3397394236b8930bb309ee03dbc08e408251
Jacob Mishkin <user@MacBook-Pro-3.local>
2019-02-14 17:54:22 -0600

    konami code art

--- sketch.js
+++ sketch.js
@@ -11,17 +11,22 @@ const sketch = () => {
   const createGrid = () => {
     const colorCount = random.rangeFloor(2, 6);
     const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount);
-    console.log(palette);
     const points = [];
     const count = 20;
+    const characters = "←↑→↓AB".split("");
+
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const character = random.pick(characters);
+        const radius = Math.abs(random.noise2D(u, v)) * 0.08;
         points.push({
           color: random.pick(palette),
-          radius: Math.abs(0.01 + random.gaussian() * 0.01),
-          position: [u, v]
+          radius,
+          rotation: random.noise2D(u, v),
+          position: [u, v],
+          character
         });
       }
     }
@@ -29,23 +34,30 @@ const sketch = () => {
   };
 
   // random.setSeed(10);
-  const points = createGrid().filter(() => random.value() > 0.5);
-  const margin = 200;
+  const points = createGrid().filter(() => random.value() > 0.2);
 
   return ({ context, width, height }) => {
-    context.fillStyle = "salmon";
+    const margin = width * 0.05;
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
     points.forEach(data => {
-      const { position, radius, color } = data;
+      const { position, radius, color, rotation, character } = data;
       const [u, v] = position;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.beginPath();
+      // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.fillStyle = color;
+      // context.fill();
+      context.save();
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px 'Helvetica'`;
+      context.translate(x, y);
+      //context.rotate(rotation);
+      context.fillText(character, 0, 0);
+      context.restore();
     });
   };
 };
