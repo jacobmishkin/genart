2631ab153dbf54d4147d941c3ddd107caad6c50b
Jacob Mishkin <user@MacBook-Pro-3.local>
2019-02-14 12:13:12 -0600

    git init

---
+++ sketch.js
@@ -0,0 +1,22 @@
+const canvasSketch = require("canvas-sketch");
+
+const settings = {
+  dimensions: [2048, 2048],
+  orientation: "landscape"
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = "orange";
+    context.fillRect(0, 0, width, height);
+
+    context.beginPath();
+    context.arc(width / 2, height / 2, 300, 0, Math.PI * 2, false);
+    context.fillStyle = "red";
+    context.fill();
+    context.lineWidth = 15;
+    context.stroke();
+  };
+};
+
+canvasSketch(sketch, settings);
