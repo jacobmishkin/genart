ec093d225e6f9d62ad483d4bf21dc71a2ff71319
Jacob Mishkin <user@MacBook-Pro-3.local>
2019-02-14 13:20:37 -0600

    added random values to canvas

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 const canvasSketch = require("canvas-sketch");
+const { lerp } = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [2048, 2048],
@@ -8,33 +10,34 @@ const settings = {
 const sketch = () => {
   const createGrid = () => {
     const points = [];
-    const count = 5;
+    const count = 60;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
-        const u = x / (count - 1);
-        const v = y / (count - 1);
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
         points.push([u, v]);
       }
     }
     return points;
   };
 
-  const points = createGrid();
-  console.log(points);
+  random.setSeed(10);
+  const points = createGrid().filter(() => random.value() > 0.5);
+  const margin = 400;
 
   return ({ context, width, height }) => {
-    context.fillStyle = "white";
+    context.fillStyle = "#bd6867";
     context.fillRect(0, 0, width, height);
 
     points.forEach(([u, v]) => {
-      const x = u * width;
-      const y = v * height;
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 50, 0, Math.PI * 2, false);
-      context.strokeStyle = "#a2eae2";
+      context.arc(x, y, 5, 0, Math.PI * 2, false);
+      context.fillStyle = "#a2eae2";
       context.lineWidth = 20;
-      context.stroke();
+      context.fill();
     });
   };
 };
